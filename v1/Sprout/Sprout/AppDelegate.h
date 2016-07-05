9eadce5fb8ef053b43ee35e5e65507aaf57c8061
Kimson Wong <kimsonwong@gmail.com>
2016-07-05 17:36:10 +0800

    for v1
    Signed-off-by: Kimson Wong <kimsonwong@gmail.com>

---
+++ v1/Sprout/Sprout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sprout
+//
+//  Created by LLDM 0038 on 05/07/2016.
+//  Copyright © 2016 sprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
